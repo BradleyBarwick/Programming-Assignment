aa8fedf831c0d7ee6ed96dbc07d7d5453207b607
BradleyBarwick <bradleybarwick@icloud.com>
2022-01-16 15:54:15 +0000

    Variable functionality complete! Beginning work on Loop functionality.

--- Programming Assignment/Script.cs
+++ Programming Assignment/Script.cs
@@ -2,30 +2,61 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Programming_Assignment
 {
 
     public class Script : Parser
     {
-        
-        Dictionary<string, string> variables = new Dictionary<string, string>();
+        Dictionary<string, string> Variables = new Dictionary<string, string>();
+
+        public void NewVariable(string line)
+        {
+            string Variable = line.Split(' ')[0].Trim();
+            string Variable2 = line.Split(' ')[2].Trim();
+            Variables[Variable] = Variable2;
+        }
+
+        public string SubstituteCommand(string command)
+        {
+            string SubstitutedCommand = command;
+
+            foreach (string key in Variables.Keys)
+            {
+                SubstitutedCommand = Regex.Replace(SubstitutedCommand, @"\b(" + key + @")\b", Variables[key].ToString(), RegexOptions.IgnorePatternWhitespace);
+            }
+            return SubstitutedCommand;
+        }
 
         public new bool ParseCommand(string input)
         {
+
             string script = input.ToLower().Trim();
             string[] commands = script.Split("\r\n");
-
+            string substitudedCommand = "";
             bool error = false;
 
             base.ResetTextOffset();
             foreach (string command in commands)
             {
-                if (!base.ParseCommand(command))
+                string[] wordSplit = command.Split(' ');
+                if (command.Contains("="))
+                {
+                    NewVariable(command);
+                    continue;
+                }
+
+                foreach (string word in wordSplit)
+                {
+                    substitudedCommand = SubstituteCommand(command);
+                }
+
+                if (!base.ParseCommand(substitudedCommand))
                 {
                     error = true;
                 }
-            }
+        }
             
             if (error)
             {

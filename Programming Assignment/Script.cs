eeed6f4c7d75589c896964ea3f17941009b9a500
BradleyBarwick <bradleybarwick@icloud.com>
2022-01-17 11:44:27 +0000

    final commit for part 2

--- Programming Assignment/Script.cs
+++ Programming Assignment/Script.cs
@@ -10,6 +10,7 @@ namespace Programming_Assignment
     {
         Dictionary<string, string> Variables = null;
         bool error = false;
+        //function that handles the setting of variables and variable addition
         private void NewVariable(string line)
         {
             string SubstitutedCommand = SubstituteCommand(line);
@@ -39,11 +40,11 @@ namespace Programming_Assignment
                 }
                 catch (Exception)
                 {
-                    base.PopulateErrors(new []{ "You can only add a number to a variable! "+ line});
+                    base.PopulateErrors(new []{ "You can only add a number to a variable! { var = var + num }: "+ line});
                 }
             }
         }
-
+        //uses regex to replace set variables (keys) in the text lines
         private string SubstituteCommand(string command)
         {
             string SubstitutedCommand = command;
@@ -54,7 +55,7 @@ namespace Programming_Assignment
             }
             return SubstitutedCommand;
         }
-
+        //the function that loops the commands a specified "times"
         private void NewLoop(string[] commands, string times)
         {
             for(int i = 0; i < int.Parse(times); i++)
@@ -79,6 +80,7 @@ namespace Programming_Assignment
                 if (command.Contains("=") && !command.Contains("=="))
                 {
                     NewVariable(command);
+                    //continue in the loop in order to stop variable declaration line from reaching base.parsecommand (and erroring out)
                     continue;
                 }
 
@@ -86,6 +88,7 @@ namespace Programming_Assignment
                 {
                     int LoopCommandIndex = 0;
                     int EndLoopCommandIndex = 0;
+                    //loop through script lines in order to find the loop command and its endloop counterpart and save the indexes in order to pick out the commands between them.
                     foreach (string loopcommand in commands)
                     {
                         if (loopcommand.Contains("loop") && !loopcommand.Equals("endloop"))
@@ -98,29 +101,46 @@ namespace Programming_Assignment
                         {
                             EndLoopCommandIndex = Array.IndexOf(commands, loopcommand);
                             commands[Array.IndexOf(commands, loopcommand)] = "";
+                            // break in order to not confuse established Indexes with later written loops
+                            break;
                         }
                     }
+                    if (EndLoopCommandIndex == 0)
+                    {
+                        base.PopulateErrors(new[] { "Loop isn't ended, \"endloop\" lines missing!"});
+                        EndLoopCommandIndex = commands.Length;
+                    }
+
+                    //store commands inbetween the loop command and endloop
                     List<string> LoopCommands = new List<string>();
+                    //since the loop command and the endloop line has been found we can select the difference between those lines and store them in the list
                     for (int i = LoopCommandIndex + 1; i < EndLoopCommandIndex; i++)
                     {
                         LoopCommands.Add(commands[i]);
                     }
+                    //pass the list through to the newloop function in order to be looped
                     NewLoop(LoopCommands.ToArray(), wordSplit[1]);
+                    //continue in order to stop loop command from reaching base.parsecommand as this would cause an error.
                     continue;
                 }
 
                 if (command.Contains("if"))
                 {
+                    //substitute the if statement line in case it contains set variables
                     string substitutedIf = SubstituteCommand(command);
+                    //get the index of the if command in the commands array
                     int commandIndex = Array.IndexOf(commands, command);
+                    //resplit the substitutedIf line appropriately
                     wordSplit = substitutedIf.Split(' ');
                     try
                     {
+                        //switch depending on the operator used in the substitutedIf line
                         switch (wordSplit[2])
                         {
                             case "==":
                                 if (wordSplit[1].Equals(wordSplit[3]))
                                 {
+                                    //commandIndex + 1 means to get the line after the initial if statement command
                                     ParseCommand(commands[commandIndex + 1]);
                                 }
                                 break;
@@ -142,33 +162,48 @@ namespace Programming_Assignment
                                     ParseCommand(commands[commandIndex + 1]);
                                 }
                                 break;
+                            case ">=":
+                                if (int.Parse(wordSplit[1]) >= int.Parse(wordSplit[3]))
+                                {
+                                    ParseCommand(commands[commandIndex + 1]);
+                                }
+                                break;
+                            case "<=":
+                                if (int.Parse(wordSplit[1]) <= int.Parse(wordSplit[3]))
+                                {
+                                    ParseCommand(commands[commandIndex + 1]);
+                                }
+                                break;
                             default:
                                 base.PopulateErrors(new[] { "Unknown Operation for If statement: " + wordSplit[2] });
                                 break;
                         }
+                        //empty the lines that should only be executed if the if condition is true and also empty the if statement line to stop base.parsecommand from seeing it
                         if (commands[commandIndex + 1] != null)
                         {
+                            //black out the line that should only be executed if the if statement is true
                             commands[commandIndex + 1] = "";
                             commands[commandIndex] = "";
                             continue;
                         }
+                        else
+                        {
+                            base.PopulateErrors(new[] { "If statement doesn't do anything: " + commands[commandIndex] });
+                        }
                     }catch (Exception)
                     {
-                        base.PopulateErrors(new[] { "Number Format Exception at: " + commands[commandIndex + 1] });
+                        base.PopulateErrors(new[] { "Number Format Exception at: " + commands[commandIndex] });
                     }
-                }
-
-                if (command.Equals("endloop"))
-                { 
-                    continue;
                 }
 
                 foreach (string word in wordSplit)
                 {
+                    // check every word of every line to see if it contains a set variable.
                     substitudedCommand = SubstituteCommand(command);
                 }
                 if (!command.Equals(""))
                 {
+                    // every line should be adequately processed before it reaches this base.parsecommand line
                     if (!base.ParseCommand(substitudedCommand))
                     {
                         error = true;
